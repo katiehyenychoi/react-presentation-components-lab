dac30fb0d036cd237829f518b325a2e2961b4ff3
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-05-01 22:53:32 -0400

    commit tree 80546f49d6b7acb3a84ce7af51148f1ad9136606

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,31 @@
+import React, { Component } from 'react';
 // Code SimpleComponent Here
+
+// const { Component } = require("react");
+
+export default class SimpleComponent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            mood: "happy"
+        }
+    }
+
+    handleClick = () => {
+        const newMood = (this.state.mood === "happy" ?
+            "sad" : "happy")
+        this.setState({
+            mood: newMood
+        })
+    }
+
+
+
+    render() {
+        return (
+            <div onClick={() => this.handleClick()}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
